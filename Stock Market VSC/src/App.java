b559dd302df2636b82b81fcd932fcad54ad3cafc
Oliver Bender <28Obender@student.penfield.edu>
2025-11-20 11:31:35 -0500

    Enhance App with money tracking and UI updates
    Added money management variables and UI components.

--- Stock Market VSC/src/App.java
+++ Stock Market VSC/src/App.java
@@ -4,6 +4,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         int windowWidth = 800;
         int windowHeight = 500;
+        int money = 500;
+        int investedWinLoss + /*something like the stock change converted to a percentage times the invested stock*/;
+        int stockMoney = money + /*investedWinLoss*/;
+
+        JLabel textLabel = new JLabel();
+        JPanel textPanel = new JPanel();
 
         JFrame frame = new JFrame("Stock Market");
         frame.setVisible(true);
@@ -15,7 +21,13 @@ public class App {
 
         StockMarket stockMarket = new StockMarket(windowWidth, windowHeight);
         frame.add(stockMarket);
-        frame.pack();    
+        frame.pack();
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 250));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setVerticalAlignment(JLabel.BOTTOM);
+        textLabel.setOpaque(true);
+        textLabel.setText("Money:" + money);
     }
 }
+
 
