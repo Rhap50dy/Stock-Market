1e181b1f0bca8365da2361cfb8d173d94e4f355f
Oliver Bender <28Obender@student.penfield.edu>
2025-11-19 20:38:30 -0500

    added more problems
    il add an issue

--- Stock Market VSC/src/RandomStock.java
+++ Stock Market VSC/src/RandomStock.java
@@ -1,14 +1,13 @@
 public class RandomStock {
     double randomUpDown = Math.random();
-    int stock = 500;
 
     public RandomStock() {
         while(true) {
             if (randomUpDown < 0.47) {
-                stock = stock - 50;
+                return(stock - 50);
                 System.out.println("stock down");
             } else {
-                stock = stock + 50;
+                return(stock + 50);
                 System.out.println("stock up");
             }
             try {
